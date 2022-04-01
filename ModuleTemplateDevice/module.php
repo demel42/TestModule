e0ce91bbcd20cd144b834409ba36c1b4dee0c820
Ch. Damsky <demel41@web.de>
2022-04-01 14:40:28 +0200

    initial

---
+++ ModuleTemplateDevice/module.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../libs/CommonStubs/common.php'; // globale Funktionen
+require_once __DIR__ . '/../libs/local.php';  // lokale Funktionen
+
+class ModuleTemplateDevice extends IPSModule
+{
+    use StubsCommonLib;
+    use ModuleTemplateLocalLib;
+
+    public function Create()
+    {
+        parent::Create();
+
+        $this->RegisterPropertyBoolean('module_disable', false);
+    }
+
+    private function CheckConfiguration()
+    {
+        $s = '';
+        $r = [];
+
+        if ($r != []) {
+            $s = $this->Translate('The following points of the configuration are incorrect') . ':' . PHP_EOL;
+            foreach ($r as $p) {
+                $s .= '- ' . $p . PHP_EOL;
+            }
+        }
+
+        return $s;
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        $vops = 0;
+
+        $refs = $this->GetReferenceList();
+        foreach ($refs as $ref) {
+            $this->UnregisterReference($ref);
+        }
+        $propertyNames = [];
+        foreach ($propertyNames as $name) {
+            $oid = $this->ReadPropertyInteger($name);
+            if ($oid >= 10000) {
+                $this->RegisterReference($oid);
+            }
+        }
+
+        $module_disable = $this->ReadPropertyBoolean('module_disable');
+        if ($module_disable) {
+            $this->SetStatus(IS_INACTIVE);
+            return;
+        }
+
+        if ($this->CheckConfiguration() != false) {
+            $this->SetStatus(self::$IS_INVALIDCONFIG);
+            return;
+        }
+
+        $this->SetStatus(IS_ACTIVE);
+    }
+
+    protected function GetFormElements()
+    {
+        $formElements = [];
+
+        $formElements[] = [
+            'type'    => 'Label',
+            'caption' => 'ModulTemplate Device'
+        ];
+
+        @$s = $this->CheckConfiguration();
+        if ($s != '') {
+            $formElements[] = [
+                'type'    => 'Label',
+                'caption' => $s
+            ];
+            $formElements[] = [
+                'type'    => 'Label',
+            ];
+        }
+
+        $formElements[] = [
+            'type'    => 'CheckBox',
+            'name'    => 'module_disable',
+            'caption' => 'Disable instance'
+        ];
+
+        return $formElements;
+    }
+
+    protected function GetFormActions()
+    {
+        $formActions = [];
+
+        $formActions[] = [
+            'type'      => 'ExpansionPanel',
+            'caption'   => 'Expert area',
+            'expanded ' => false,
+            'items'     => [
+                [
+                    'type'    => 'Button',
+                    'caption' => 'Re-install variable-profiles',
+                    'onClick' => 'WMS_InstallVarProfiles($id, true);'
+                ],
+            ],
+        ];
+
+        $formActions[] = [
+            'type'      => 'ExpansionPanel',
+            'caption'   => 'Test area',
+            'expanded ' => false,
+            'items'     => [
+                [
+                    'type'    => 'TestCenter',
+                ],
+            ]
+        ];
+
+        $formActions[] = $this->GetInformationForm();
+        $formActions[] = $this->GetReferencesForm();
+
+        return $formActions;
+    }
+
+    public function RequestAction($ident, $value)
+    {
+        if ($this->CommonRequestAction($ident, $value)) {
+            return;
+        }
+
+        if ($this->GetStatus() == IS_INACTIVE) {
+            $this->SendDebug(__FUNCTION__, 'instance is inactive, skip', 0);
+            return;
+        }
+
+        $this->SendDebug(__FUNCTION__, 'ident=' . $ident . ', value=' . $value, 0);
+
+        $r = false;
+        switch ($ident) {
+            default:
+                $this->SendDebug(__FUNCTION__, 'invalid ident ' . $ident, 0);
+                break;
+        }
+        if ($r) {
+            $this->SetValue($ident, $value);
+        }
+    }
+}
