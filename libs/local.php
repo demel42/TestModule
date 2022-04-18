7610d0004a707479c8bcbcb14a44724fda6fa7fb
Ch. Damsky <demel41@web.de>
2022-04-18 15:34:09 +0200

    Anpassung

--- libs/local.php
+++ libs/local.php
@@ -4,29 +4,13 @@ declare(strict_types=1);
 
 trait TestModuleLocalLib
 {
-    public static $IS_INVALIDPREREQUISITES = IS_EBASE + 1;
-    public static $IS_UPDATEUNCOMPLETED = IS_EBASE + 2;
-    public static $IS_INVALIDCONFIG = IS_EBASE + 3;
-    public static $IS_DEACTIVATED = IS_EBASE + 4;
-
     public static $STATUS_INVALID = 0;
     public static $STATUS_VALID = 1;
     public static $STATUS_RETRYABLE = 2;
 
     private function GetFormStatus()
     {
-        $formStatus = [
-            ['code' => IS_CREATING, 'icon' => 'inactive', 'caption' => 'Instance getting created'],
-            ['code' => IS_ACTIVE, 'icon' => 'active', 'caption' => 'Instance is active'],
-            ['code' => IS_DELETING, 'icon' => 'inactive', 'caption' => 'Instance is deleted'],
-            ['code' => IS_INACTIVE, 'icon' => 'inactive', 'caption' => 'Instance is inactive'],
-            ['code' => IS_NOTCREATED, 'icon' => 'inactive', 'caption' => 'Instance is not created'],
-
-            ['code' => self::$IS_INVALIDPREREQUISITES, 'icon' => 'error', 'caption' => 'Instance is inactive (invalid preconditions)'],
-            ['code' => self::$IS_UPDATEUNCOMPLETED, 'icon' => 'error', 'caption' => 'Instance is inactive (update not completed)'],
-            ['code' => self::$IS_INVALIDCONFIG, 'icon' => 'error', 'caption' => 'Instance is inactive (invalid configuration)'],
-            ['code' => self::$IS_DEACTIVATED, 'icon' => 'inactive', 'caption' => 'Instance is inactive (deactivated)'],
-        ];
+        $formStatus = $this->GetCommonFormStatus();
 
         return $formStatus;
     }
